4c25c2f7bd506bef7e93a0378d4a0b4453615f42
wahjh <896723014@qq.com>
2019-04-30 15:11:22 +0800

    2019-4-30 15:11:05卢智鹏  完善人事管理模块功能和类型

--- ssm_web/src/main/java/com/ssm/wuye/controller/SysAdminInfoController.java
+++ ssm_web/src/main/java/com/ssm/wuye/controller/SysAdminInfoController.java
@@ -10,7 +10,6 @@ package com.ssm.wuye.controller;
 import com.ssm.wuye.domain.SysAdminInfo;
 import com.ssm.wuye.domain.SysAdminInfoExample;
 import com.ssm.wuye.service.SysAdminInfoService;
-import org.apache.ibatis.session.RowBounds;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
