e74983feecff9d4a293a3588892ee6c0f1aabe0e
cspiderman <chenzefengboy@Sina.com>
2019-05-05 10:47:00 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	ssm_dao/src/main/java/com/ssm/wuye/dao/NewsTypeVoiwSqlProvider.java
    #	ssm_dao/src/main/java/com/ssm/wuye/dao/ParkingPlaceinfoMapper.java
    #	ssm_dao/src/main/java/com/ssm/wuye/domain/ParkingPlaceinfo.java
    #	ssm_service/ssm.log
    #	ssm_web/src/main/java/com/ssm/wuye/controller/ContractController.java
    #	ssm_web/src/main/webapp/pages/huoduan/index.jsp

---
+++ ssm_dao/src/main/java/com/ssm/wuye/dao/ActivityMapper.java
@@ -0,0 +1,68 @@
+package com.ssm.wuye.dao;
+
+import com.ssm.wuye.domain.Activity;
+import com.ssm.wuye.domain.ActivityExample;
+import java.util.List;
+import org.apache.ibatis.annotations.DeleteProvider;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.InsertProvider;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
+import org.apache.ibatis.annotations.SelectProvider;
+import org.apache.ibatis.annotations.UpdateProvider;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.type.JdbcType;
+
+public interface ActivityMapper {
+    @SelectProvider(type=ActivitySqlProvider.class, method="countByExample")
+    long countByExample(ActivityExample example);
+
+    @DeleteProvider(type=ActivitySqlProvider.class, method="deleteByExample")
+    int deleteByExample(ActivityExample example);
+
+    @Insert({
+        "insert into activity (id, activityname, ",
+        "activitydesc, activitycont, ",
+        "activitytime, activityworker, ",
+        "activityward)",
+        "values (#{id,jdbcType=INTEGER}, #{activityname,jdbcType=VARCHAR}, ",
+        "#{activitydesc,jdbcType=VARCHAR}, #{activitycont,jdbcType=VARCHAR}, ",
+        "#{activitytime,jdbcType=DATE}, #{activityworker,jdbcType=VARCHAR}, ",
+        "#{activityward,jdbcType=VARCHAR})"
+    })
+    int insert(Activity record);
+
+    @InsertProvider(type=ActivitySqlProvider.class, method="insertSelective")
+    int insertSelective(Activity record);
+
+    @SelectProvider(type=ActivitySqlProvider.class, method="selectByExample")
+    @Results({
+        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER),
+        @Result(column="activityname", property="activityname", jdbcType=JdbcType.VARCHAR),
+        @Result(column="activitydesc", property="activitydesc", jdbcType=JdbcType.VARCHAR),
+        @Result(column="activitycont", property="activitycont", jdbcType=JdbcType.VARCHAR),
+        @Result(column="activitytime", property="activitytime", jdbcType=JdbcType.DATE),
+        @Result(column="activityworker", property="activityworker", jdbcType=JdbcType.VARCHAR),
+        @Result(column="activityward", property="activityward", jdbcType=JdbcType.VARCHAR)
+    })
+    List<Activity> selectByExampleWithRowbounds(ActivityExample example, RowBounds rowBounds);
+
+    @SelectProvider(type=ActivitySqlProvider.class, method="selectByExample")
+    @Results({
+        @Result(column="id", property="id", jdbcType=JdbcType.INTEGER),
+        @Result(column="activityname", property="activityname", jdbcType=JdbcType.VARCHAR),
+        @Result(column="activitydesc", property="activitydesc", jdbcType=JdbcType.VARCHAR),
+        @Result(column="activitycont", property="activitycont", jdbcType=JdbcType.VARCHAR),
+        @Result(column="activitytime", property="activitytime", jdbcType=JdbcType.DATE),
+        @Result(column="activityworker", property="activityworker", jdbcType=JdbcType.VARCHAR),
+        @Result(column="activityward", property="activityward", jdbcType=JdbcType.VARCHAR)
+    })
+    List<Activity> selectByExample(ActivityExample example);
+
+    @UpdateProvider(type=ActivitySqlProvider.class, method="updateByExampleSelective")
+    int updateByExampleSelective(@Param("record") Activity record, @Param("example") ActivityExample example);
+
+    @UpdateProvider(type=ActivitySqlProvider.class, method="updateByExample")
+    int updateByExample(@Param("record") Activity record, @Param("example") ActivityExample example);
+}
